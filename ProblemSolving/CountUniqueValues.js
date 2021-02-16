412e70f319a2d710203b9e5511263c1b638658bb
warrenniu <warrenniu@gmail.com>
2021-02-16 12:42:31 -0500

    solving monotonic array problem

--- ProblemSolving/CountUniqueValues.js
+++ ProblemSolving/CountUniqueValues.js
@@ -5,34 +5,40 @@
 //countUniqueValues([]) //0
 //countUniqueValues([1,2,3,4,4,7,7,12,12,13]) //7
 
-function countUniqueValues(arr) {
-    // Create two pointers, one being at the zeroth index and the second at the first index
-    let i = 0
-    let j = 1
+// function countUniqueValues(arr) {
+//     // Create two pointers, one being at the zeroth index and the second at the first index
+//     let i = 0
+//     let j = 1
     
-    while (arr === undefined || arr.length === 0) {
-        return 0
-    }
-    while (j === arr.length) {
-        return 1
-    }
-    console.log(arr)
+//     while (arr === undefined || arr.length === 0) {
+//         return 0
+//     }
+//     while (j === arr.length) {
+//         return 1
+//     }
+//     console.log(arr)
 
 
-    while (j < arr.length) {
-        if (arr[i] === arr[j]) {
-            j++
-        }
-        else if (arr[i] !== arr[j]) {
-            i++
-            arr[i] = arr[j]
-        }
-    }
-    let uniqueArray = [... new Set(arr)]
-    console.log(uniqueArray)
-    return uniqueArray.length
-}
+//     while (j < arr.length) {
+//         if (arr[i] === arr[j]) {
+//             j++
+//         }
+//         else if (arr[i] !== arr[j]) {
+//             i++
+//             arr[i] = arr[j]
+//         }
+//     }
+//     let uniqueArray = [... new Set(arr)]
+//     console.log(uniqueArray)
+//     return uniqueArray.length
+// }
 
+//===============================================================================
+//Implement a function called countUniqueValues, which accepts a sorted array, and counts the unique values in the array. There can be negative numbers in the array, but it will always be sorted
+//===============================================================================
+//countUniqueValues([1,1,1,1,2]) //2
+//countUniqueValues([]) //0
+//countUniqueValues([1,2,3,4,4,7,7,12,12,13]) //7
 
 
 // Colt Steele method
@@ -50,4 +56,23 @@ function countUniqueValues(arr) {
 //     return i + 1
 // }
 
-console.log(countUniqueValues([]))
+//===============================================================================
+//Implement a function called countUniqueValues, which accepts a sorted array, and counts the unique values in the array. There can be negative numbers in the array, but it will always be sorted
+//===============================================================================
+//countUniqueValues([1,1,1,1,2]) //2
+//countUniqueValues([]) //0
+//countUniqueValues([1,2,3,4,4,7,7,12,12,13]) //7
+
+function countUniqueValues(arr) {
+    let i = 0
+    for (j = 1; j < arr.length; j++) {
+        if (arr[j] !== arr[i]) {
+            i++
+            arr[i] = arr[j]
+        }
+        console.log(arr)
+    }
+    return i + 1
+}
+
+console.log(countUniqueValues([1]))
