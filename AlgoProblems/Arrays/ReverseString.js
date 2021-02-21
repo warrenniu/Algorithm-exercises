a227c9bb4332f1154299da2e40266beacd1df5c8
warrenniu <warrenniu@gmail.com>
2021-02-21 14:18:54 -0500

    solved reverseString problem

--- AlgoProblems/Arrays/ReverseString.js
+++ AlgoProblems/Arrays/ReverseString.js
@@ -1,3 +1,9 @@
+// function reverseString(s) {
+
+//     let reverseArray = s.reverse()
+//     return reverseArray
+// }
+
 //===============================================================================
 //Write a function that reverses a string. The input string is given as an array of characters char[]. Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory. You may assume all the characters consist of printable ascii characters
 //===============================================================================
@@ -5,32 +11,28 @@
 //Constraints
 //1. O(1) space complexity
 
-//Steps (Multiple Pointers)
-//1. Set left pointer to 0; right pointer to last index
-//2. With each iteration, set a temp variable to the value of the left pointer set left pointer to right pointer
-//3. return s at end of function
+//Steps
+//1. Set up the two pointers - one at index 0 and one at the last index
+//2. Use a while loop - set condition to left (0 index) < right (last index)
+//3. With each iteration, we'll be moving the left pointer up one and right pointer down one
+//4. Create a temp variable that stores the value of the left pointer
+//5. Set the value of the left pointer to the right pointer
+//6. Set the valuee of the right pointer to the temp variable
+//7. At the end of the function, return the array
 
-// function reverseString(s) {
-
-//     let reverseArray = s.reverse()
-//     return reverseArray
-// }
-
-//Two Pointers approach
-function reverseString(s) {
+function reverseString(arr) {
     let left = 0;
-    let right = s.length - 1;
+    let right = arr.length - 1;
 
     while (left < right) {
-        let temp = s[left]
-        s[left] = s[right]
-        s[right] = temp 
+        let temp = arr[left]
+        arr[left] = arr[right]
+        arr[right] = temp
         left++
         right--
     }
-    return s
+    return arr
 }
-
 
 
 
