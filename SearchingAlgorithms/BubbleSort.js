7ef7c443d3bfcdd3fd0125aa19684fce6f8bf9bf
warrenniu <warrenniu@gmail.com>
2021-03-12 09:21:28 -0500

    added bubble sort. working on selection sort

--- SearchingAlgorithms/BubbleSort.js
+++ SearchingAlgorithms/BubbleSort.js
@@ -38,27 +38,68 @@
 //     return count
 // }
 
-function naiveString(long,short) {
-    let count = 0;
+// function naiveString(long,short) {
+//     let count = 0;
 
-    for (let i = 0; i < long.length; i++) {
-        for (let j = 0; j < short.length; j++) {
-            console.log(short[j],long[i+j])
-            if (short[j] !== long[i+j]) {
-                console.log("break!")
-                break
-            }
-            if (j === short.length - 1) {
-                console.log("Found One")
-                count++
-            }
+//     for (let i = 0; i < long.length; i++) {
+//         for (let j = 0; j < short.length; j++) {
+//             console.log(short[j],long[i+j])
+//             if (short[j] !== long[i+j]) {
+//                 console.log("break!")
+//                 break
+//             }
+//             if (j === short.length - 1) {
+//                 console.log("Found One")
+//                 count++
+//             }
 
+//         }
+//     }
+//     return count
+// }
+
+// console.log(naiveString('zomgzomg', 'omg'))
+
+//BubbleSort Pseudocode
+
+//Steps
+//1. Start looping from with a variable called i at the end of the array towards the beginning
+//2. Start an inner loop with a variable called j from the beginning until i - 1
+//3. If arr[j] is greater than arr[j+1], swap those two values
+//4. Return the sorted array
+
+function bubbleSort(arr) {
+    let noSwaps; //stop the loop after we've finished sorting so there isnt any needless iterations
+    for (let i = arr.length; i > 0; i--) {
+        noSwaps = true
+        // console.log(arr[i])
+        for (let j = 0; j < i - 1 ; j++) {
+            console.log(arr,arr[j], arr[j+1])
+            if (arr[j] > arr[j+1]) {
+                // [arr[j], arr[j+1], [arr[j+1], arr[j]]]
+                let temp = arr[j+1]
+                arr[j+1] = arr[j]
+                arr[j] = temp
+                noSwaps = false
+            }
+        }
+        if (noSwaps) {
+            break
+            
+            // console.log(arr[j],arr[j+1])
+            // console.log(arr[i])
         }
     }
-    return count
+    return arr
 }
 
-console.log(naiveString('zomgzomg', 'omg'))
+console.log(bubbleSort([5,7,18,9,12,24,1]))
+
+
+
+
+
+
 
 // for (let i = 0; i < str1.length; i++) {
 //     for (let j = 0; j < str2.length; j++) {
