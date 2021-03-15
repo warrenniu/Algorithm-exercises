112e0f76472ef9f6359eef186433ebdd62298aaa
warrenniu <warrenniu@gmail.com>
2021-03-15 10:48:15 -0400

    added insertion sort

---
+++ SearchingAlgorithms/InsertionSort.js
@@ -0,0 +1,23 @@
+// Pseudocode:
+
+// - Start by picking the second element in the array
+// - Now compare the second element with the one before it and swap if necessary
+// - Continue to the next element and if it is in the incorrect order, iterate through the sorted portion (i.e. the left side) to place the element in the correct place
+// - Repeat until the array is sorted
+
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        // console.log(arr[i])
+        let currentVal = arr[i];
+        for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+            console.log(j)
+            arr[j+1] = arr[j] 
+        }
+        arr[j+1] = currentVal
+        console.log(arr)
+
+    }
+    return arr
+}
+
+console.log(insertionSort([2,1,9,76,4]))
