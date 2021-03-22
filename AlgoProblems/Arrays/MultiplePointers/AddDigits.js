341c1b9c03fce236ee6c815764c77b6b82eeffbc
warrenniu <warrenniu@gmail.com>
2021-03-22 18:15:14 -0400

    solving adddigits problem

--- AlgoProblems/Arrays/MultiplePointers/AddDigits.js
+++ AlgoProblems/Arrays/MultiplePointers/AddDigits.js
@@ -7,34 +7,68 @@
 //2. 0 <= num <= (2 ** 31) - 1))
 
 //Steps
-//1. Convert the num to a string
-//2. Create a sum variable to store the current sum
-//2. If string.length is equal to one, return it and parse it to Int
-//3. Loop over the string. Parse each to Int and add str[i] + add str[i+1] and set that equal to sum
-//4. increment i by 2
-//5. Return sum
+//1. Create a variable called currentSum = 0
+//2. Convert the num to a string and set it to a variable called numString
+//3. Set up a while loop. If length of numString > 1
+//4. Set up a nested for loop. set currentSum = currentSum + numString[i]
 
-function sumDigits(num) {
-    let digits = num.toString().split('');
-    let sum = 0;
 
-    for (let i = 0; i < digits.length; i++) {
-        sum += Number(digits[i])
-    }
-    return sum
-}
 
 function addDigits(num) {
+    let currentSum = 0;
+    let numString = num.toString().split('');
+    console.log(numString)
 
-    num = sumDigits(num)
+    if (numString.length > 1) {
+        console.log('inside if statement')
 
-    if (`${num}`.length === 1) { return num }
+        for (let i = 0; i < numString.length; i++) {
+            currentSum = currentSum + parseInt(numString[i])
+            console.log(currentSum)
+            addDigits(currentSum)
+        }
+        return currentSum
+        
+        // numString = currentSum.toString().split('')
+        // console.log(numString)
+    }
+  
 
-    return addDigits(num)
-
-
-
+    else if (numString.length <= 1) {
+        return parseInt(numString)
+    }
+    // else if (numString.length <= 1) {
+    // return currentSum
+    // }
 }
 
 
-console.log(addDigits(383))
+
+
+
+console.log(addDigits(38))
+
+
+// function sumDigits(num) {
+//     let digits = num.toString().split('');
+//     let sum = 0;
+
+//     for (let i = 0; i < digits.length; i++) {
+//         sum += Number(digits[i])
+//     }
+//     return sum
+// }
+
+// function addDigits(num) {
+
+//     num = sumDigits(num)
+
+//     if (`${num}`.length === 1) { return num }
+
+//     return addDigits(num)
+
+
+
+// }
+
+
--- SearchingAlgorithms/MergeSort.js
+++ SearchingAlgorithms/MergeSort.js
@@ -1,20 +1,7 @@
-//===============================================================================
-//Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
-//===============================================================================
-
-//Constraints
-// The number of nodes in both lists is in the range [0, 50].
-// -100 <= Node.val <= 100
-// Both l1 and l2 are sorted in non-decreasing order.
-
 //Steps
-//1. Set up newArray
-//2. i = 0, left = 0, right = 0
-//3. Set up two pointers. One at the first index of first array - other at the first index of the second array
-//4. Set up a while loop. While i is less than arr1.length OR i is less than arr2.length
-//5. If statement - if arr1[left] less than arr2[right], push arr1[left] & left++
-//6. Else if - if arr1[left] is greater than arr2[right], push arr2[right] & right++
-//7. Return newArray
+//1. Break up the array into halves until you have arrays that are empty or have one element
+//2. Once you have smaller sorted arrays, merge those arrays with other sorted arrays until you are back at the full length of the array
+//3. Once the array has been merged back together, return the merged (and sorted!) array
 
 function mergeTwoLists(arr1, arr2) {
     let newArray = [];
@@ -66,4 +53,13 @@ function mergeTwoLists(arr1, arr2) {
 
 
 
-console.log(mergeTwoLists([], [0]))
+function mergeSort(arr) {
+    if (arr.length <= 1) return arr;
+    let mid = Math.floor(arr.length / 2);
+    let left = mergeSort(arr.slice(0,mid));
+    let right = mergeSort(arr.slice(mid));
+
+    return mergeTwoLists(left, right)
+
+
+}
