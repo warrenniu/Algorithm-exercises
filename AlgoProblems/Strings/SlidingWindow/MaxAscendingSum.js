171925749ce1747ee5d5b79307465b864851fc18
warrenniu <warrenniu@gmail.com>
2021-04-06 10:52:02 -0400

    added maxAscendingSum problem. Couldn't solve - had to lookup solution

--- AlgoProblems/Strings/SlidingWindow/MaxAscendingSum.js
+++ AlgoProblems/Strings/SlidingWindow/MaxAscendingSum.js
@@ -6,44 +6,60 @@
 // 1 <= nums.length <= 100
 // 1 <= nums[i] <= 100
 
-//Sliding Window approach
-
 //Steps
-//1. Create a variable called sum, set it equal to nums[0]
-//2. Set up two pointers for window -> trailing = 0, leading = 1
-//3. Set up a while loop where trailing is less than or equal to leading. If nums[trailing] < nums[leading] then add two together and increment leading by 1
-//4. If nums[trailing] > nums[leading], then move trailing to leading and move leading up by 1. Set conditional -> if currentSum is > then sum, then set sum = currentSum. *need to somehow reset currentSum to 1 or trailing value
-//5. Return sum at end of function
+//1. Create a exmpty empty to store the sums of the values
+//2. Set two variables called max & sum which will track the current sum of the subarray, and max which will check the max value between the current sum & the max value being pushed into the array
+//3. If the value of i+1 is less than the current iteration, we'll reset the max and sum value to 0
+
+// function maxAscendingSum(nums) {
+//     let sum = 0
+//     let trailing = 0;
+//     let leading = 0;
+    
+//     while (trailing <= leading && nums.length > 1) {
+//         let currentSum = 0;
+//         console.log("in while loop")
+//         if (trailing === nums.length - 1) {
+//             break
+//         }
+//         else if (nums[leading] < nums[leading + 1]) {
+//             console.log("nums[trailing]", nums[trailing], "nums[leading", nums[leading])
+//             leading++
+//         }
+//         else if (nums[leading] > nums[leading + 1] || leading === nums.length - 1) {
+//             console.log("nums[trailing]", nums[trailing], "nums[leading", nums[leading])
+
+//             for (let i = trailing; i <= leading; i++) {
+//                 currentSum += nums[i]
+//             }
+//             if (currentSum > sum) {
+//                 sum = currentSum
+//             }
+//             trailing = leading + 1
+//             leading++
+//         }
+//     }
+// return sum
+// }
 
 function maxAscendingSum(nums) {
-    let sum = 0
-    let trailing = 0;
-    let leading = 0;
-    
-    while (trailing <= leading && nums.length > 1) {
-        let currentSum = 0;
-        console.log("in while loop")
-        if (trailing === nums.length - 1) {
-            break
-        }
-        else if (nums[leading] < nums[leading + 1]) {
-            console.log("nums[trailing]", nums[trailing], "nums[leading", nums[leading])
-            leading++
-        }
-        else if (nums[leading] > nums[leading + 1] || leading === nums.length - 1) {
-            console.log("nums[trailing]", nums[trailing], "nums[leading", nums[leading])
+    let max = 0;
+    let sum = 0;
+    let newArray = [];
 
-            for (let i = trailing; i <= leading; i++) {
-                currentSum += nums[i]
-            }
-            if (currentSum > sum) {
-                sum = currentSum
-            }
-            trailing = leading + 1
-            leading++
+    for (let i = 0; i< nums.length; i++) {
+        sum += nums[i]
+        max = Math.max(max, sum)
+        console.log(newArray)
+        newArray.push(max)
+
+        if (nums[i+1] <= nums[i]) {
+            sum = 0;
+            max = 0;
         }
     }
-return sum
+    console.log(newArray)
+    return Math.max(...newArray)
 }
 
 console.log(maxAscendingSum([10,20,30,5,10,50]))
