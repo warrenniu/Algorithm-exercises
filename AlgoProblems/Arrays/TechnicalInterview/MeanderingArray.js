6c5e4bcde0192f4140e1df577ac9b4c8ba5d64dd
warrenniu <warrenniu@gmail.com>
2021-04-19 15:25:08 -0400

    wrote blog on meandering array

--- AlgoProblems/Arrays/TechnicalInterview/MeanderingArray.js
+++ AlgoProblems/Arrays/TechnicalInterview/MeanderingArray.js
@@ -1,27 +1,30 @@
+
+
 function meanderingArr(arr){
 
     let finalArr = []
-    let descArr = [...arr].sort((a,b) => a - b )
-    // let ascArr = [...arr].sort((a,b) => a - b)
+    let sortedArr = arr.sort((a,b) => a - b )
+    console.log(sortedArr)
     let flag = true;
     let small = 0
     let big = arr.length - 1
 
-
-    console.log(descArr)
-
     for(let i = 0; i<arr.length; i++){
         if(flag) {
-            finalArr[i] = descArr[big--]
+            finalArr[i] = sortedArr[big--]
+            
         }else{
-            finalArr[i] = descArr[small++]
+            finalArr[i] = sortedArr[small++]
+            
         }
-
+    
         flag = !flag
     }
 
-
-
-    return finalArr
+return finalArr
 
 }
+
+
+
+console.log(meanderingArr([-1,3,4,6,-5]))
